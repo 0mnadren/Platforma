3fb40a2f0fb1e513ebb0ca46b3a5a29bbc8ffefa
Vukasin <vikasinkurandic@gmail.com>
2021-08-17 17:08:57 +0200

    Test 2 Vukasin

--- recenzentPlatforma/settings.py
+++ recenzentPlatforma/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         "HOST": "localhost",
         "PORT": "3306",
         "USER": "root",
-        "PASSWORD": "12345678"
+        "PASSWORD": ""  # Promenjena Sifra
     }
 }
 
