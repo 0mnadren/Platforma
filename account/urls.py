918552105017047e734d358c435143ed11b4983f
zyle47 <nemanja.zivkovic47@gmail.com>
2021-08-24 12:58:55 +0200

    Login Napravljen <3

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
+
+from django.conf.urls.static import static
+from django.conf import settings
+
 app_name = 'account'
 
 urlpatterns = [
@@ -10,4 +14,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='account/logout.html'), name='logout'),
     path('status/', views.status, name='status'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
