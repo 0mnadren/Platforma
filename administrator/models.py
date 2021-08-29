87e8799c808ea3bf54cca16b8dd2bf90d9944ba4
Nemanja Davidović <neuritzerg@gmail.com>
2021-08-29 16:32:10 +0200

    Pregled prijava(Admin)
    Ubacena kolona koliko je trenutno radova na recenziji i
    Promenjen model Obavestenje -- naslov vise nije unique

--- administrator/models.py
+++ administrator/models.py
@@ -6,7 +6,7 @@ from profil.models import Profil
 class Obavestenje(models.Model):
     profil = models.ManyToManyField(Profil)
 
-    naslov = models.CharField(max_length=60, unique=True)
+    naslov = models.CharField(max_length=60)
     tekst = models.TextField()
     datum_vreme_kreiranja = models.DateTimeField(auto_now_add=True)
     potpis = models.CharField(max_length=60)
