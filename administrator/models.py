09b97bba082b43a527ca87922b2e403852589f76
zyle47 <nemanja.zivkovic47@gmail.com>
2021-09-01 18:08:12 +0200

    Update Zile
    Obavestenja, search i sort
    Pregled prijava sort

--- administrator/models.py
+++ administrator/models.py
@@ -1,16 +1,26 @@
 from django.db import models
+from django.db.models.base import Model
 
 from profil.models import Profil
 
 
 class Obavestenje(models.Model):
-    profil = models.ManyToManyField(Profil)
+    profil = models.ManyToManyField(Profil, through='ObrisanoStanje')
 
     naslov = models.CharField(max_length=60)
     tekst = models.TextField()
     datum_vreme_kreiranja = models.DateTimeField(auto_now_add=True)
     potpis = models.CharField(max_length=60)
 
+
     def __str__(self):
         return self.naslov
+    
+class Obrisanostanje(models.Model):
+    profil = models.ForeignKey(Profil, on_delete=models.CASCADE, related_name='korisnik')
+    obavestenje = models.ForeignKey(Obavestenje, on_delete=models.CASCADE, related_name='obavest')
+    obrisano = models.BooleanField(default=False)
+    
 
+    def __str__(self):
+        return f'{self.profil} + {self.obrisano}'
--- profil/urls.py
+++ profil/urls.py
@@ -5,6 +5,7 @@ app_name = 'profil'
 
 urlpatterns = [
     path('', views.profil, name='profil'),
+    path('search/', views.search, name='search'),
     path('<int:pk>/obrisi/', views.obrisi_obavestenje, name='obrisi_obavestenje'),
     path('prijava/', views.prijava, name='prijava'),
 ]
