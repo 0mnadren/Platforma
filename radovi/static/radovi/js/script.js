7a7388b47ec746d6fffad87a1799a80fac9ddc1e
Nemanja Davidović <neuritzerg@gmail.com>
2021-09-10 12:30:42 +0200

    Bug fixing 2
    1. Postojao je propust, gde je admin mogao da napravi programski poziv, ali ne i pitanja za taj isti i da ubaci takav programski poziv u novi rad i posalje recenzentima na ocenjivanje bez pitanja, sto bi onda dovelo do greske.
    2. Ubacio sam js u naucni_rad_admin --> pitanja su sada sakrivena

---
+++ radovi/static/radovi/js/script.js
@@ -0,0 +1,20 @@
+const btnPrikazi = document.getElementById('btn_prikazi');
+const btnSakrij = document.getElementById('btn_sakrij');
+const divPitanja = document.getElementById('programski_poziv_pitanja');
+
+window.addEventListener('load', function() {
+    btnSakrij.style.display = 'none';
+    divPitanja.style.display = 'none';
+});
+
+btnPrikazi.addEventListener('click', function() {
+    btnPrikazi.style.display = 'none';
+    btnSakrij.style.display = 'block';
+    divPitanja.style.display = 'block'
+});
+
+btnSakrij.addEventListener('click', function() {
+    btnPrikazi.style.display = 'block';
+    btnSakrij.style.display = 'none';
+    divPitanja.style.display = 'none';
+});
