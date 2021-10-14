ffc59c0a306cca6515cc47b48ce975995d690856
Nemanja Davidović <neuritzerg@gmail.com>
2021-10-14 14:29:19 +0200

    Update urls.py
    Added django.conf.urls.i18n in urlpatterns

--- recenzentPlatforma/urls.py
+++ recenzentPlatforma/urls.py
@@ -20,7 +20,9 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = []
+urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
+]
 
 urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
